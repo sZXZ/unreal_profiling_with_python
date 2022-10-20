4325a2fe216bd767556a01ec5e555ac25e5d090a
alexander.prokopchuk <alexander.prokopchuk@vizor-games.com>
2022-10-20 16:54:01 +0300

    init profiling plugin

---
+++ profiling_project/Plugins/Profiling/Source/Profiling/Private/ProfilingBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProfilingBPLibrary.h"
+#include "Profiling.h"
+
+UProfilingBPLibrary::UProfilingBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UProfilingBPLibrary::ProfilingSampleFunction(float Param)
+{
+	return -1;
+}
+
