c6be9d59bb68020c2b76d0ff4f37a447adc53ae3
alexander.prokopchuk <alexander.prokopchuk@vizor-games.com>
2022-10-25 16:55:46 +0300

    adds python profiling utils

---
+++ utils/render_hierarchy_to_html.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+import pandas as pd
+import re
+
+
+def build_hierarchy(object, space_count):
+    time_str = datetime.now().strftime('%H%M%S')
+    values = f"var object_{time_str} = {object}; var space_{time_str} = {space_count}; const htmlgraphicsValue_{time_str} = document.getElementById('htmlgraphics-value_{time_str}');"
+    javascript = """function refresh_INDICATOR(){htmlNode_INDICATOR=document;function s(s,e){if(object_INDICATOR[e].includes("FRAME")||object_INDICATOR[e].includes("Culled to"))return-1;check_arr=s.slice().reverse().slice(-1*e);for(var r=0;r<check_arr.length;r++)if(check_arr[r]<s[e])return check_arr.length-r-1;return 0}parent_index_INDICATOR=[];for(var e=0;e<space_INDICATOR.length;e++)parent_index_INDICATOR.push(s(space_INDICATOR,e));function r(s,e){children=[];for(var r=0;r<s.length;r++)s[r]!=e||visited[r]||(children.push(r),visited[r]=!0);return children}string_INDICATOR="",prefix='<span style="visibility: hidden">',postfix="</span>",obj_INDICATOR={},visited=Array(parent_index_INDICATOR.length).fill(!1),htmlgraphicsValue_INDICATOR.innerHTML="";for(var t=0;t<object_INDICATOR.length;t++){null==htmlNode_INDICATOR.getElementById("id_INDICATOR_"+t)&&(htmlgraphicsValue_INDICATOR.innerHTML+='<ul id="id_INDICATOR_'+t+'" style="list-style-type: disclosure-closed;"><li>'+object_INDICATOR[t]+"</li></ul>"),display=r(parent_index_INDICATOR,t);for(var i=0;i<display.length;i++)display[i]!=t&&(htmlNode_INDICATOR.getElementById("id_INDICATOR_"+t).innerHTML+='<ul id="id_INDICATOR_'+display[i]+'" style="display: none;list-style-type: disclosure-closed;"><li>'+object_INDICATOR[display[i]]+"</li></ul>")}for(t=0;t<object_INDICATOR.length;t++)htmlNode_INDICATOR.getElementById("id_INDICATOR_"+t).getElementsByTagName("li")[0].onclick=function(s){my_parent=this.parentElement,ul_tags=my_parent.getElementsByTagName("ul"),state=my_parent.style.listStyleType,hid=!1;for(var e=0;e<ul_tags.length;e++)(ul_tags[e].parentElement==my_parent||s.shiftKey)&&(vis=ul_tags[e].style.display,"disclosure-open"==state&&(hid=!0,s.shiftKey&&(ul_tags[e].style.listStyleType="disclosure-closed"),ul_tags[e].style.display="none"),"disclosure-closed"==state&&(hid=!1,s.shiftKey&&(ul_tags[e].style.listStyleType="disclosure-open"),ul_tags[e].style.display="block"));hid?my_parent.style.listStyleType="disclosure-closed":my_parent.style.listStyleType="disclosure-open"}}refresh_INDICATOR();"""
+    style = """*{font-family:Roboto,Helvetica,Arial,sans-serif}.box{border:solid #555 2px;border-radius:10px;padding:10px 20px;}"""
+    html = f"""<style>{style}</style><div><div class="box" id="htmlgraphics-value_{time_str}"></div></div><script>{values}{javascript.replace('INDICATOR', time_str)}</script>"""
+    return html
+
+def parse_profile_gpu(row):
+    percentage, data = row['lines'].split('%')
+    data = data.split()
+    row['ms'] = float(data[0][:-2])
+    row['name'] = data[1]
+    values_to_parse = {
+        'draws':['draw'],
+        'prims':['prim'], 
+        'verts':['vert'], 
+        'dispatches':['dispatch'], 
+        'groups':['group']
+    }
+    for val in values_to_parse:
+        row[val] = data[data.index(val)-1] if val in data else '0'
+        for alt_name in values_to_parse[val]:
+            row[val] = data[data.index(alt_name)-1] if alt_name in data else row[val]
+        if 'x' in row[val]:
+            row[val] = row[val].split('x')
+    return row
+
+def profile_gpu(log):
+    lines = []
+    spaces = []
+    values = []
+    is_profilegpu = False
+    for line in log:
+        if 'LogRHI' in line:
+            clean_line = re.sub(r'\[.{24}\[...]\w+: ', '', line)
+            if 'FRAME' in line:
+                is_profilegpu = True
+            if 'Total Nodes' in line:
+                is_profilegpu = False
+            if is_profilegpu:
+                value = float(clean_line.split()[0].split('%')[0])
+                values.append(value)
+                lines.append(clean_line)
+                add = 0
+                if value > 10.0 and not 'FRAME' in line:
+                    add = 1
+                spaces.append(len(clean_line) - len(clean_line.lstrip()) + add)
+    df = pd.DataFrame({"lines": lines, "spaces": spaces, "values": values})
+    df = df.apply(parse_profile_gpu, axis=1)
+    return build_hierarchy(log['lines'].to_list(), log['spaces'].to_list()), df
+
+
+def profile_cpu(log):
+    lines = []
+    spaces = []
+    values = []
+    is_profile = False
+    for line in log:
+        if 'LogStats' in line:
+            add = 0
+            clean_line = re.sub(r'\[.{24}\[...]\w+: ', '', line)
+            if 'Culled to' in line:
+                add = -100
+                is_profile = True
+            if is_profile:
+                value = 0
+                try:
+                    value = float(clean_line.split()[0].split('ms')[0])
+                except:
+                    pass
+                if value > 10.0 and not 'Culled to' in line:
+                    add = 1
+                values.append(value)
+                spaces.append(len(clean_line) -
+                              len(clean_line.lstrip()) + add)
+                lines.append(clean_line)
+    log = pd.DataFrame({"lines": lines, "spaces": spaces, "values": values})
+    return build_hierarchy(log['lines'].to_list(), log['spaces'].to_list())
---
+++ utils/session_ctrl.py
@@ -0,0 +1,303 @@
+from time import sleep, time
+from pathlib import Path
+import shutil
+import pandas as pd
+from utils.render_hierarchy_to_html import profile_gpu, profile_cpu
+from IPython.display import display, HTML
+from remote_execution import RemoteExecution
+
+# -----------------------------------------------------------------------
+# common data gather functions
+
+
+def tail(file, n):
+    from collections import deque
+    log = file
+    with open(log) as fin:
+        return list(deque(fin, n))
+
+
+def gather_all_csv_charts_median_in_folders():
+    data_frames = []
+    for p in Path.cwd().glob(f'data/*/csvprofile*'):
+        header_at_end = tail(p, 2)[0].split(',')
+        df = pd.read_csv(
+            p, skipfooter=2,
+            engine='python', on_bad_lines='skip',
+            names=header_at_end, skiprows=1
+        ).median()
+        data_frames.append(df.to_frame(p.parent.name))
+    df = data_frames[0].join(data_frames[1:]).T
+    return df
+
+
+def gather_all_fps_charts_median_in_folders():
+    data_frames = []
+    for p in Path.cwd().glob(f'data/*/*-fps*.csv'):
+        df = pd.read_csv(p, skiprows=4)
+        med = df[df.columns.to_list()[:-1]].median().to_frame(p.parent.name)
+        data_frames.append(med)
+    df = data_frames[0].join(data_frames[1:]).T
+    return df
+
+
+class UnrealEngineConnection():
+    def __init__(self, project_saved_folder, project_log, do_init=True, project_commands_plugin='ProfilingBPLibrary'):
+        """
+        UnrealEngineRemoteExecution class for running tests in session frontend 
+        make sure to select session in unreal engine
+        expected commands command plugin:
+        send_command(FString) -> sends console command to a selected Session in session frontend
+        """
+        self.project_saved_folder = project_saved_folder
+        self.project_name = project_saved_folder.split('\\')[-2]
+        self.project_log = project_log
+
+        self.remote_exec = RemoteExecution()
+        self.remote_exec.start()
+        self.data_folder = Path.cwd().joinpath('data')
+        self.project_commands_plugin = project_commands_plugin
+        if not self.data_folder.exists():
+            self.data_folder.mkdir()
+        sleep(1)
+        for node in self.remote_exec.remote_nodes:
+            if node['project_name'] in self.project_name:
+                self.remote_exec.open_command_connection(node.get("node_id"))
+        if do_init:
+            self.init_build_setup()
+
+    def rc(self, command):
+        if self.remote_exec.has_command_connection:
+            result = self.remote_exec.run_command(
+                f"unreal.{self.project_commands_plugin}.send_command('{command}')")
+        else:
+            result = 'no connection'
+        return result
+
+    def reconnect(self):
+        if self.remote_exec.has_command_connection:
+            self.remote_exec.close_command_connection()
+        for node in self.remote_exec.remote_nodes:
+            self.remote_exec.open_command_connection(node.get("node_id"))
+
+    def close_connection(self):
+        if self.remote_exec.has_command_connection:
+            self.remote_exec.close_command_connection()
+
+    def init_build_setup(self):
+        self.rc('t.FPSChart.DoCsvProfile 1')
+        self.rc('stat unit')
+        self.rc('r.VSync 0')
+        self.rc('t.FPSChart.OpenFolderOnDump 0')
+        pass
+
+    def count_objects(self):
+        self.rc('obj list')
+        sleep(1)
+        data = []
+        log = tail(self.project_log, 500)
+        last_profile = 0
+        for i, line in enumerate(log):
+            if ']Obj List: ' in line:
+                last_profile = i
+            if last_profile > 0:
+                data = log[last_profile:]
+        df = pd.DataFrame(data[3:-2])
+        df = df[0].str.split(expand=True)
+        df.columns = df.iloc[0]
+        df = df.drop(0)
+        df = df.drop(df.columns[[0]], axis=1)
+        df['Count'] = df['Count'].astype("int")
+        df = df.sort_values(ascending=False)
+        return df
+
+    def read_profile_gpu(self, name='', read_for=1000):
+        """
+        reads last 'read_for' lines of log to find profilegpu
+        result gets copied to clipboard
+        returns DataFrame - no hierarchy 
+        """
+        log = []
+        gpu_data = []
+        last_profilegpu = 0
+        log = tail(self.project_log, read_for)
+        for i, line in enumerate(log):
+            if 'Profiling the next GPU frame' in line:
+                last_profilegpu = i
+        gpu_data = log[last_profilegpu:]
+        previous_results = Path(f'{name}.html')
+        if previous_results.exists():
+            previous_results.rename(Path(f'{name}_{int(time())}.html'))
+        if last_profilegpu != 0:
+            html, df = profile_gpu(gpu_data)
+            with open(f'{self.data_folder.resolve()}{name}.html', 'w') as fr:
+                fr.write(html)
+            display(HTML(html))
+            return df
+        else:
+            print('no profile information found')
+            return pd.DataFrame()
+
+    def run_profile_gpu(self, name='', read_for=1000):
+        """
+        runs profilegpu command
+        reads last 'read_for' lines of log to find profilegpu
+        result gets copied to clipboard
+        returns DataFrame - no hierarchy 
+        """
+        self.rc('stat unit')
+        sleep(0.1)
+        self.rc('profilegpu')
+        sleep(0.8)
+        self.rc('stat unit')
+        return self.read_profile_gpu(name, read_for)
+
+    def run_profile_cpu(self, name=''):
+        """
+        runs DumpFrame command
+        reads last 500 lines of log to find DumpFrame
+        result gets copied to clipboard
+        """
+        self.rc('stat unit')
+        sleep(0.1)
+        self.rc('stat DumpFrame')
+        sleep(0.8)
+        self.rc('stat unit')
+        log = []
+        gpu_data = []
+        last_profile = 0
+        log = tail(self.project_log, 500)
+        for i, line in enumerate(log):
+            if 'Single Frame ' in line:
+                last_profile = i
+        gpu_data = log[last_profile:]
+        previous_results = Path(f'{name}.html')
+        if previous_results.exists():
+            previous_results.rename(Path(f'{name}_{int(time())}.html'))
+        if last_profile != 0:
+            html = profile_cpu(gpu_data)
+            with open(f'{self.data_folder.resolve()}{name}.html', 'w') as fr:
+                fr.write(html)
+            display(HTML(html))
+        else:
+            print('no profile information found')
+
+    def run_memreport(self, test_name):
+        """
+        runs command and moves all memreport data from saved folder to data folder
+        """
+        self.rc('memreport -full')
+        sleep(1)
+        saved = Path(self.project_saved_folder)
+        stats = saved.joinpath('profiling', 'memreports')
+        path_for_tests = self.data_folder
+        possible_paths = stats.iterdir()
+        found_folders = 0
+        if stats.exists():
+            for index, p in enumerate(possible_paths):
+                shutil.copytree(p, path_for_tests.joinpath(
+                    f"memreport_{test_name}_{index}"))
+                shutil.rmtree(p)
+                found_folders += 1
+        else:
+            print("Can't find memreports")
+        if found_folders == 1:
+            path_for_tests.joinpath(f"memreport_{test_name}_0").rename(
+                path_for_tests.joinpath(f"memreport_{test_name}"))
+
+    def move_fps(self, test_name):
+        """
+        moves all fps chart data from saved folder
+        """
+        saved = Path(self.project_saved_folder)
+        log = saved.joinpath('logs', f'{self.project_name}.log')
+        fps_charts = saved.joinpath('profiling', 'fpschartstats')
+        path_for_tests = self.data_folder
+        possible_paths = fps_charts.iterdir()
+        found_folders = 0
+        paths = []
+        if fps_charts.exists():
+            for index, p in enumerate(possible_paths):
+                shutil.copytree(p, path_for_tests.joinpath(
+                    f"{test_name}_{index}"))
+                shutil.rmtree(p)
+                paths.append(path_for_tests.joinpath(
+                    f"{test_name}_{index}"))
+                found_folders += 1
+        else:
+            print("Can't find fpschart")
+        if found_folders == 1:
+            path_for_tests.joinpath(f"{test_name}_0").rename(
+                path_for_tests.joinpath(f"{test_name}"))
+            return [path_for_tests.joinpath(f"{test_name}")]
+        return paths
+
+    def do_fps_chart(self, seconds, test_name: str, wait_for_file=False,
+                     interesting_stats=['FrameTime', 'GPUTime',
+                                        'RenderThreadTime', 'GameThreadTime',
+                                        'RHIThreadTime', 'RHI/DrawCalls',
+                                        'RHI/PrimitivesDrawn']):
+        """
+        runs fps chart and moves data from saved folder to data folder
+        return: Full Csv Chart DataFrame, median interesting_stats DataFrame, median (FrameTime) 
+        """
+        shutil.rmtree(Path.cwd().joinpath(test_name), ignore_errors=True)
+        self.rc('stat unit')
+        sleep(0.1)
+        self.rc("startfpschart")
+        sleep(seconds)
+        self.rc("stopfpschart")
+        sleep(0.1)
+        self.rc('stat unit')
+        if wait_for_file:
+            sleep(3)
+        folders_with_charts = self.move_fps(test_name)
+        for folder in folders_with_charts:
+            for p in folder.glob(f'data/*/csvprofile*'):
+                header_at_end = tail(p, 2)[0].split(',')
+                df = pd.read_csv(
+                    p, skipfooter=2, engine='python', on_bad_lines='skip', names=header_at_end, skiprows=1)
+                med = df[interesting_stats].median().to_frame(test_name)
+                return df, med, med.loc[['FrameTime'], :][test_name][0]
+        return pd.DataFrame(), 0, 0
+
+    def do_simple_fps_chart(self, seconds, test_name: str, wait_for_file=False):
+        """
+        runs fps chart and moves data from saved folder to data folder
+        return: Full DataFrame, median interesting_stats DataFrame, median (FrameTime) 
+        """
+        shutil.rmtree(Path.cwd().joinpath(test_name), ignore_errors=True)
+        self.rc('stat unit')
+        sleep(0.1)
+        self.rc("startfpschart")
+        sleep(seconds)
+        self.rc("stopfpschart")
+        sleep(0.1)
+        self.rc('stat unit')
+        if wait_for_file:
+            sleep(3)
+        folders_with_charts = self.move_fps(test_name)
+        for folder in folders_with_charts:
+            for p in folder.glob(f'data/*/*-fps*.csv'):
+                df = pd.read_csv(p, skiprows=4)
+                med = df[df.columns.to_list()[:-1]].median().to_frame(test_name)
+                return df, med, df['Frame (ms)'].median()
+        return pd.DataFrame(), 0, 0
+
+    # -----------------------------------------------------------------------
+    # techart_tools project specific commands
+
+    def open_level(self, level_name:str):
+        self.rc(f'open {level_name}')
+
+    def open_level_with_gamemode(self, level_name:str, gamemode_path:str):
+        self.rc(f'open {level_name}?game={gamemode_path}')
+
+    def stream_level_in(self, level_path:str):
+        self.rc(f'streamLevelIn {level_path}')
+
+    def stream_level_out(self, level_path:str):
+        self.rc(f'streamLevelOut {level_path}')
+
+    def do_enchanted_input_action(self, action:str):
+        self.rc(f'input.+action {action}')
