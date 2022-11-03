1219f5933e6a28af91bc9aad134aabb7a0e75503
alexander.prokopchuk <alexander.prokopchuk@vizor-games.com>
2022-11-03 13:04:34 +0200

    run any windows project with script

--- profiling_project/Script/run_on_external_machine.bat
+++ profiling_project/Script/run_on_external_machine.bat
@@ -1,1 +1,2 @@
-profiling_project.exe -Messaging -SessionName="Build" -execcmds="session AUTH username"
+for %%f in (*.exe) do %%f -stdout -utf8output -Messaging -SessionName="NameThatWillBeShownInFrontend" -execcmds="session AUTH username_of_controlling_pc"
+pause
