63984f71179a45f55557ec1da8959756a3a24c70
alexander.prokopchuk <alexander.prokopchuk@vizor-games.com>
2022-11-09 14:59:30 +0200

    enable profilegpu in test builds

--- profiling_project/Source/profiling_project.Target.cs
+++ profiling_project/Source/profiling_project.Target.cs
@@ -10,5 +10,11 @@ public class profiling_projectTarget : TargetRules
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.AddRange( new string[] { "profiling_project" } );
+        
+        if (Configuration == UnrealTargetConfiguration.Test)
+        {
+            GlobalDefinitions.Add("ALLOW_PROFILEGPU_IN_TEST=1");
+            bUseLoggingInShipping = true;
+        }
 	}
 }
